14e776db1dee046d7047cc1e57d715a577e29b4c
nereaalcolecha <nealma@alumnatflorida.es>
2025-09-15 12:22:03 +0200

    Ejercicio cambiado

--- bloque_1/01_refuerzo_php/Actividades/AP1-1/ap1-1.php
+++ bloque_1/01_refuerzo_php/Actividades/AP1-1/ap1-1.php
@@ -1,12 +1,12 @@
 <?php
 
-$datos = array();
+$datos = [];
 
-if (isset($_GET)){
+if (!empty($_GET)) {
     $datos = $_GET;
-}
 
-foreach ($datos as $clave => $valor) {
-    echo "Se ha recibido $valor para la clave $clave";
+    foreach ($datos as $clave => $valor) {
+        echo "Se ha recibido $valor para la clave $clave.<br>";
+    }
 }
 ?>
