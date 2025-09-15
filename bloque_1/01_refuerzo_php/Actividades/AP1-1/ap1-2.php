fb90666cf976f4b1d9f81f66c3dd95d00dcc302c
nereaalcolecha <nealma@alumnatflorida.es>
2025-09-15 12:24:00 +0200

    Ejercicio cambiado

--- bloque_1/01_refuerzo_php/Actividades/AP1-1/ap1-2.php
+++ bloque_1/01_refuerzo_php/Actividades/AP1-1/ap1-2.php
@@ -2,22 +2,21 @@
 
 $valores = [];
 
-
-if(empty($_GET)) {
-    echo "Se ha recibido valor nulo<br>";
+if (empty($_GET)) {
+    echo "No se ha recibido ningún dato o es un valor nulo<br>";
 } else {
     foreach ($_GET as $clave => $valor) {
         $valores[$clave] = $valor;
     }
+
     foreach ($valores as $clave => $valor) {
-        if ($valor == is_numeric($valor)) {
-            echo " Se ha recibido un número <br>";
-        } elseif ($valor == "null") {
-            echo " No se ha recibido ningún dato o es un valor nulo <br>";
-        } elseif ($valor == is_string($valor)) {
-            echo " Se ha recibido una cadena de caracteres <br>";
+        if ($valor === "" || strtolower($valor) === "null") {
+            echo "No se ha recibido ningún dato o es un valor nulo<br>";
+        } elseif (is_numeric($valor)) {
+            echo "Se ha recibido un número<br>";
+        } else {
+            echo "Se ha recibido una cadena de caracteres<br>";
         }
     }
 }
-
 ?>
