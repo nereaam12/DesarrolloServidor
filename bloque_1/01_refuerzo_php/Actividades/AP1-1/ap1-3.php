fe589b93f6fa0f6b861934bb3b7e5ef34ac8f841
nereaalcolecha <nealma@alumnatflorida.es>
2025-09-15 12:30:42 +0200

    Ejercicio cambiado

--- bloque_1/01_refuerzo_php/Actividades/AP1-1/ap1-3.php
+++ bloque_1/01_refuerzo_php/Actividades/AP1-1/ap1-3.php
@@ -11,32 +11,33 @@ $posicion = [
 
 $sum = 0;
 
-$puesto = ;
-
-foreach ($puesto as $num => $p){
-    if($p == "primero" || $p == "tercero" || $p == "quinto"){
+foreach ($posicion as $num => $p) {
+    if (in_array($p, ["primero", "tercero", "quinto"], true)) {
         $sum += $num;
-        echo " Estas en una posicion impar $sum <br> ";
-        if($sum > 20){
-            echo " El valor de la suma es mayor que 20 <br> ";
-        }elseif ($sum > 10){
-            echo " El valor de la suma es mayor que 10 <br> ";
-        }elseif ($sum > 5){
-            echo " El valor de la suma es mayor que 5 <br>";
-        }else echo " El valor de la suma no es mayor <br>";
+        echo "Estás en una posición impar $sum <br>";
+        if ($sum > 20) {
+            echo "El valor de la suma es mayor que 20 <br>";
+        } elseif ($sum > 10) {
+            echo "El valor de la suma es mayor que 10 <br>";
+        } elseif ($sum > 5) {
+            echo "El valor de la suma es mayor que 5 <br>";
+        } else {
+            echo "El valor de la suma no es mayor <br>";
+        }
         $impar = true;
         $par = false;
-    }
-    elseif ($p == "segundo" || $p == "cuarto" || $p == "sexto"){
+    } elseif (in_array($p, ["segundo", "cuarto", "sexto"], true)) {
         $sum += $num;
-        echo " Estas en una posicion par $sum <br> ";
-        if($sum > 20){
-            echo " El valor de la suma es mayor que 20 <br>";
-        }elseif ($sum > 10){
-            echo " El valor de la suma es mayor que 10 <br> ";
-        }elseif ($sum > 5){
-            echo " El valor de la suma es mayor que 5 <br>";
-        }else echo " El valor de la suma no es mayor <br> ";
+        echo "Estás en una posición par $sum <br>";
+        if ($sum > 20) {
+            echo "El valor de la suma es mayor que 20 <br>";
+        } elseif ($sum > 10) {
+            echo "El valor de la suma es mayor que 10 <br>";
+        } elseif ($sum > 5) {
+            echo "El valor de la suma es mayor que 5 <br>";
+        } else {
+            echo "El valor de la suma no es mayor <br>";
+        }
         $impar = false;
         $par = true;
     }
